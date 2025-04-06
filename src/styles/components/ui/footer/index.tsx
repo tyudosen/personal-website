8671503246ad6596ea5a9ae432edbdd7ed9e6288
tyudosen <tyudosen@gmail.com>
2025-04-05 19:52:20 -0500

    change footer project button text

--- src/styles/components/ui/footer/index.tsx
+++ src/styles/components/ui/footer/index.tsx
@@ -5,7 +5,7 @@ import { Button } from "../button";
 export default function Footer() {
   return <footer className="flex gap-4">
     <Button className="font-black" variant="link">
-        Personal Projects
+        Projects
     </Button>
     <Button className="font-black" variant="link">
         Contact Me
