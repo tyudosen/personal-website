dec820a3ad5a20b29c8bab20c874c24f95645726
tyudosen <tyudosen@gmail.com>
2025-04-07 16:54:50 -0500

    update fathom code

--- src/app/fathom.tsx
+++ src/app/fathom.tsx
@@ -1,34 +1,29 @@
-'use client';
+'use client'
+import { load, trackPageview } from 'fathom-client'
+import { useEffect } from 'react'
+import { usePathname, useSearchParams } from 'next/navigation'
 
-import { load, trackPageview } from 'fathom-client';
-import { useEffect, Suspense } from 'react';
-import { usePathname, useSearchParams } from 'next/navigation';
-
-function TrackPageView() {
-  const pathname = usePathname();
-  const searchParams = useSearchParams();
-  
-
+export default function Fathom() {
+  const pathname = usePathname()
+  const searchParams = useSearchParams()
   useEffect(() => {
-    load(process.env.NEXT_PUBLIC_FATHOM_ID as string);
-  }, []);
+    load(process.env.NEXT_PUBLIC_FATHOM_ID as string, {
+      includedDomains: [process.env.DOMAIN_ONE as string, process.env.DOMAIN_TWO as string],
+      spa: 'auto',
+    })
 
-  useEffect(() => {
-    if (!pathname) return;
-
+    // Initialize Fathom when the app loads
+    // Example: yourdomain.com
+    //  - Do not include https://
+    //  - This must be an exact match of your domain.
+    //  - If you're using www. for your domain, make sure you include that here.
     trackPageview({
       url: pathname + searchParams?.toString(),
       referrer: document.referrer
-    });
-  }, [pathname, searchParams]);
+    })
 
-  return null;
-}
+    // Record a pageview when route changes
+  }, [pathname, searchParams])
 
-export function FathomAnalytics() {
-  return (
-    <Suspense fallback={null}>
-      <TrackPageView />
-    </Suspense>
-  );
+  return null
 }
