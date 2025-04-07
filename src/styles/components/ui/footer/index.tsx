72b801858804d5f1d21cb5c52d88ae57a1d6d46d
tyudosen <tyudosen@gmail.com>
2025-04-07 15:34:06 -0500

    integrate fathom analytics and add a click event on the contact me link

--- src/styles/components/ui/footer/index.tsx
+++ src/styles/components/ui/footer/index.tsx
@@ -1,13 +1,18 @@
 "use client";
 
+import { trackEvent } from "fathom-client";
 import { Button } from "../button";
 
 export default function Footer() {
+  const handleClick = () => {
+    trackEvent('contact-me-link-click');
+  };
+
   return <footer className="flex gap-4">
     {/* <Button className="font-black" variant="link">
         Projects
     </Button> */}
-    <Button asChild variant="link">
+    <Button asChild variant="link" onClick={handleClick}>
     <a href="mailto:tyudosen@yahoo.com" className="font-black" rel="noopener noreferrer" target="_blank">Contact Me</a>
     </Button>
   </footer>;
