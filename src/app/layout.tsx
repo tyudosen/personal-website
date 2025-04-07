c44e8bba67c3ffae597f60ea8ddd4fff72cb5fc8
tyudosen <tyudosen@gmail.com>
2025-04-07 17:07:51 -0500

    update fathom code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter, Playfair_Display } from "next/font/google";
 import "./globals.css";
-import { FathomAnalytics } from "./fathom";
+import FathomAnalytics from "./fathom";
 const inter = Inter({
   subsets: ["latin"],
   variable: "--font-inter",
