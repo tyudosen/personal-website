1064a9d478cd8f64e1d9a850e82faf3b674cf5b6
tyudosen <tyudosen@gmail.com>
2025-04-07 16:16:51 -0500

    update fathom code

--- src/app/fathom.tsx
+++ src/app/fathom.tsx
@@ -7,11 +7,10 @@ import { usePathname, useSearchParams } from 'next/navigation';
 function TrackPageView() {
   const pathname = usePathname();
   const searchParams = useSearchParams();
+  
 
   useEffect(() => {
-    load(process.env.NEXT_PUBLIC_FATHOM_ID || '', {
-      auto: false
-    });
+    load(process.env.NEXT_PUBLIC_FATHOM_ID as string);
   }, []);
 
   useEffect(() => {
