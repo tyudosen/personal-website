32e7a2c74cc6f67889fc42d414217b1277c889ca
tyudosen <tyudosen@gmail.com>
2025-04-07 19:44:37 -0500

    update cd workflow

--- src/app/fathom.tsx
+++ src/app/fathom.tsx
@@ -1,15 +1,17 @@
 'use client'
+
 import { load, trackPageview } from 'fathom-client'
 import { Suspense, useEffect } from 'react'
 import { usePathname, useSearchParams } from 'next/navigation'
 
-console.log(process.env.NEXT_PUBLIC_FATHOM_ID)
+console.log('outside -->', process.env.NEXT_PUBLIC_FATHOM_ID)
 
 function TrackPageView() {
   const pathname = usePathname()
   const searchParams = useSearchParams()
   
   useEffect(() => {
+    console.log('inside -->', process.env.NEXT_PUBLIC_FATHOM_ID)
     load(process.env.NEXT_PUBLIC_FATHOM_ID as string, {
       includedDomains: [process.env.DOMAIN_ONE as string, process.env.DOMAIN_TWO as string],
       spa: 'auto',
